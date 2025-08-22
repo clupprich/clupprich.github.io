f96dbb94ec7355369fdc995c03954db963716891
Christoph Lupprich <christoph@luppri.ch>
2025-08-22 06:02:08 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://clupprich.github.io",
+  site: "https://christoph.luppri.ch",
 
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
